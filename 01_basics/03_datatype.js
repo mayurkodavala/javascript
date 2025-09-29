323b35c0491ad3c3f052d39454e0f9afc1a144c4
mayurkodavala <mayurkodavala07@gmail.com>
2025-09-29 06:47:48 +0000

    datatype and  ECMA standerds

---
+++ 01_basics/03_datatype.js
@@ -0,0 +1,17 @@
+let name = "Mayur"
+let age = 20
+let isLoggedIn = true
+let state;
+console.table([name,age,isLoggedIn,state]);
+
+
+//number 
+//bigInt
+//string => ""
+//boolean => true/false
+//numm => stand alone value
+//undefined => empty value
+//symbol => unique 
+//object
+
+console.log(typeof undefined);
