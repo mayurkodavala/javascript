7aeceae11d83a1f2ebc0b0ff1a477f370f666862
mayurkodavala <mayurkodavala07@gmail.com>
2025-09-29 06:25:48 +0000

    let const var variable

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,9 @@
+const accountId = 121212
+let accountEmail = "mayur.com"
+var accountPassword = "123456"
+accountCity = "ahmedabad"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
