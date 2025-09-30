ad363502f0260c632cae624d441611ca549a29a3
mayurkodavala <mayurkodavala07@gmail.com>
2025-09-30 04:45:47 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "77abc"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "77" => 77
+// "77abc" => Nan
+// true => 1, false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 77
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
