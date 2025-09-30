b7e0e767fbecde373b6c866186aa0357d2dcd9a3
mayurkodavala <mayurkodavala07@gmail.com>
2025-09-30 05:13:19 +0000

    String to Number convet

---
+++ 01_basics/04_StingtoNumber.js
@@ -0,0 +1,21 @@
+let value = 7
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " mayur"
+let str3 = str1 + str2
+// console.log(str3);
+
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2" + 3);
+console.log(1 + 2 + "3");
+console.log(1 + "2" + 3); */
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
