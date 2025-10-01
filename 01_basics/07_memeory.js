ba563a0d995a46815c58f75ce8e507f5ef70b9f6
mayurkodavala <mayurkodavala07@gmail.com>
2025-10-01 05:18:32 +0000

    stack and heap

---
+++ 01_basics/07_memeory.js
@@ -0,0 +1,20 @@
+let myName = "mayur"
+let anotherName = myName
+
+anotherName = "anuj"
+console.log(myName);
+
+console.log(anotherName);
+
+let user = {
+    email : "mayur@gpay",
+    upi : "user@bl"
+}
+console.log(user);
+
+let user1 = user
+user1.email = "mayur@google.com"
+console.log(user.email);
+console.log(user1.email);
+
+
