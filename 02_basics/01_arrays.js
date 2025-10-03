5f53e36096b8d848fdc73a042caf854f2c9e371b
mayurkodavala <mayurkodavala07@gmail.com>
2025-10-03 05:14:26 +0000

    basics array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+const myArrays = [0,1,2,3,4,5,true,"mayur"]
+// const myheros = ["balveer", "shaktiman", "chota bheem"]
+// console.log(myArrays);
+// console.log(myheros);
+
+//Array methods
+
+// myArrays.push(6)
+// myArrays.pop()
+// myArrays.shift()
+// myArrays.unshift(9)
+// console.log(myArrays.includes(9));
+// console.log(myArrays.indexOf(5));
+
+// const newArrray = myArrays.join()
+
+// console.log(newArrray);
+
+// console.log(myArrays);
+
+// slice, spslice
+
+console.log("A ", myArrays);
+
+const myn1 = myArrays.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArrays);
+
+const myn2 = myArrays.splice(1,3)
+console.log("C ", myArrays);
+console.log(myn2);
+
+
+
+
