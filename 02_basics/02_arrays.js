c3d625d6744177ed2230b039b079792f931578a8
mayurkodavala <mayurkodavala07@gmail.com>
2025-10-04 05:41:37 +0000

    methods in array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -12,15 +12,17 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 const another_array = [1,2,3,[4,5,6],7,[6,7],[4,5]]
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
-console.log(Array.isArray("mayur"))
-console.log(Array.from("mayur"))
-console.log(Array.from({name : "mayur"}))
+// console.log(Array.isArray("mayur"))
+// console.log(Array.from("mayur"))
+// console.log(Array.from({name : "mayur"}))
 
 
 let Score1 = 100
 let Score2 = 200
 let Score3 = 300
 
-console.log(Array.of(Score1,Score2,Score3));
+// console.log(Array.of(Score1,Score2,Score3));
+
+
---
+++ 02_basics/03_arrays.js
@@ -0,0 +1,27 @@
+const nums = [1,2,3,4]
+const double = nums.map(n => n * 2)  //return is new array
+
+// console.log(double);
+
+
+const nums2 = [10,20,30]
+const filtered = nums2.filter(n => n > 15)
+
+// console.log(filtered);
+
+const nums3 = [10,20,30]
+const reduce = nums3.reduce((a,b) => a + b)
+
+// console.log(reduce);
+
+const arr = [0,1,2,3,4,5]
+arr.splice(1,2)
+
+// console.log(arr);
+
+const arr1 = [1,2,3,4,5]
+arr1.slice(1,2)
+
+console.log(arr1);
+
+
