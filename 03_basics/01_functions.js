0d3220ad49ff58910af392f4d1d2a9769f23ac56
mayurkodavala <mayurkodavala07@gmail.com>
2025-10-06 05:26:44 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,7 +29,33 @@ function loginUserMassage(username = "anuj") {
     return `${username} just logged in`
 }
 // console.log(loginUserMassage("mayur"));
-console.log(loginUserMassage());
+// console.log(loginUserMassage());
+
+function calculateCartPrice(val1,val2,...num1) {
+    return num1
+}
+console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username : "mayur",
+    price : 80
+}
+function handleObject(anyObject) {
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// handleObject(user)
+
+handleObject({
+    username : "anuj",
+    price : 85
+})
+
+const mynewArr = [200,400,500,600]
+function returnSecoundValue(getArray) {
+    return getArray[1]
+}
+console.log(returnSecoundValue(mynewArr));
 
 
- 
+
