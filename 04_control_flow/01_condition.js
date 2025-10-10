5127e8aea15054071ab0fb539dccf90fabf09ee2
kodavala Mayur <mayurkodavala07@gmail.com>
2025-10-10 17:04:48 +0000

    control flow in javaScript

--- 04_control_flow/01_condition.js
+++ 04_control_flow/01_condition.js
@@ -1,40 +1,60 @@
 /* = oprator assignment /* /*== comparison  === strict comparison*/
 
-// const temprature = 40;
+// const isUserLoggedIn = true
 
-// if (temprature > 35) {
-//     console.log("true");
+// <, >, <=, >=, !=, !==, ==, ===
+
+// if(3 === "3") {
+//     console.log(true);
     
-// } else {
-//     console.log("false");
 // }
 
 
-
-// let score = 200;
-// if (score < 100) {
-//     let power = "fly";
-//     console.log(`you are hero and you can ${power}`);
+// const temprature  = 40
+// if (temprature < 50) {
+//     console.log("wear a t-shirt");
 // } else {
-//     let power = "nothing";
-//     console.log(`you are hero and you can ${power}`);
-// }
-
-// let balance = 999;
-// if(balance > 500) {
-//     console.log("buy a new phone");
-// } else {
-//     console.log("save money");
+//     console.log("wear a jacket");
 // }
 
 
-let balance = 999;
-if (balance < 500) {
-    console.log("less than 500");
-} else if (balance < 700) {
-    console.log("less than 700");
-} else if (balance < 900) {
-    console.log("more than 900");
-} else {
-    console.log("more than 1000");
+// const score = 500
+// if(score > 400) {
+//     const medal = "gold"
+//     console.log(`Congratulations! You have won a ${medal} medal.`);
+// }
+
+
+// const balance = 999
+// if(balance < 1000) {
+//     console.log("you are eligible for a credit card");
+// } else {
+//     const remainingBalance = 1000 - balance
+//     console.log(`You need ${remainingBalance} more to get a credit card`);
+// }
+
+
+// const balance  = 1000
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance > 950) {
+//     console.log("less than 950");
+// } else {
+//     console.log("less than 1200"); 
+// }
+
+
+const isUserLoggedIn = true 
+const DebitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn && DebitCard && 2 == 2) {
+    console.log("you are eligible to buy");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail || isUserLoggedIn) {
+    console.log("user logged in");
 }
