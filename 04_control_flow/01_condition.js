38278c3380ba571dbb6117d7d92adc637889a1fb
kodavala Mayur <mayurkodavala07@gmail.com>
2025-10-10 05:52:27 +0000

    condition

---
+++ 04_control_flow/01_condition.js
@@ -0,0 +1,40 @@
+/* = oprator assignment /* /*== comparison  === strict comparison*/
+
+// const temprature = 40;
+
+// if (temprature > 35) {
+//     console.log("true");
+    
+// } else {
+//     console.log("false");
+// }
+
+
+
+// let score = 200;
+// if (score < 100) {
+//     let power = "fly";
+//     console.log(`you are hero and you can ${power}`);
+// } else {
+//     let power = "nothing";
+//     console.log(`you are hero and you can ${power}`);
+// }
+
+// let balance = 999;
+// if(balance > 500) {
+//     console.log("buy a new phone");
+// } else {
+//     console.log("save money");
+// }
+
+
+let balance = 999;
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 700) {
+    console.log("less than 700");
+} else if (balance < 900) {
+    console.log("more than 900");
+} else {
+    console.log("more than 1000");
+}
