d2e48b7556d4b6ebb0f03a1b44501745ade643a8
kodavala Mayur <mayurkodavala07@gmail.com>
2025-10-11 05:35:13 +0000

    basic while and do while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,23 @@
+// While loop
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`Value of Index is ${i}`);
+//     i = i + 2
+// }   
+
+
+// let myArr = ["mayur","anuj","rohan","mohit"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr++;
+// }
+
+//Do while loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
